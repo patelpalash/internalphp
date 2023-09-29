4e4ef016dfdebd52878f2217ea8ebc65f9cc9702
palash <patelpalash57@gmail.com>
2023-09-29 21:16:30 +0530

    with unix journal but no update working

--- update.php
+++ update.php
@@ -2,22 +2,13 @@
     include 'connect.php';
     $id = $_GET['updateid'];
 
-    $sql = "Select * from `crud` where id = $id";
-    $result = mysqli_query($con,$sql);
-
-    $row = mysqli_fetch_assoc($result);
-    $name = $row['name'];
-    $email = $row['email'];
-    $mobile = $row['mobile'];
-    $password = $row['password'];
-
     if(isset($_POST['submit'])){
         $name =$_POST['name'];
         $email=$_POST['email'];
         $mobile=$_POST['mobile'];
         $password=$_POST['password'];
 
-        $sql ="UPDATE `crud` SET `name`='$name',`email`='$email',`mobile`='$mobile',`password`='$password' WHERE `id` = '$id'";
+        $sql ="UPDATE `crud` SET id=$id,name='$name',email='$email',mobile='$mobile',password='$password' WHERE id = '$id'";
         $result = mysqli_query($con,$sql);
 
         if($result){
